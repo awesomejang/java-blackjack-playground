94ed4ee34f3f715a92ceeae00d2b6799571b0de9
awesomejang <uljang1995@gmail.com>
2023-01-25 23:34:13 +0900

    test : firstBlackJekEvent 테스트코드 추가

--- src/test/java/nextstep/blackjack/RuleTest.java
+++ src/test/java/nextstep/blackjack/RuleTest.java
@@ -1,3 +1,5 @@
+package nextstep.blackjack;
+
 import nextstep.blackjack.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -7,10 +9,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RuleTest {
-
-
-
-
     @Test
     @DisplayName("딜러와 플레이어의 카드를 확인하여 게임머니를 계산한다.")
     void judgeWinnerTest() {
@@ -43,5 +41,30 @@ public class RuleTest {
         Assertions.assertThat(gamer1.getPlayerMoney().getResultMoney()).isEqualTo(10000);
         Assertions.assertThat(gamer2.getPlayerMoney().getResultMoney()).isEqualTo(-20000);
         Assertions.assertThat(overLimitGamer.getPlayerMoney().getResultMoney()).isEqualTo(-30000);
+    }
+
+    @Test
+    @DisplayName("처음 두 장의 카드 합이 21(블랙잭)일 경우 배팅금액의 1.5배를 딜러에게 받는다.")
+    void firstBlackJekEventTest() {
+        // given
+        Rule rule = new Rule();
+        Dealer dealer = new Dealer();
+        Gamer gamer1 = new Gamer("gamer1", 10000);
+        Gamer gamer2 = new Gamer("gamer2", 20000);
+
+        // when
+        gamer1.receiveCard(new Card(Card.Pattern.CLOVER, Card.Denomination.QUEEN));
+        gamer1.receiveCard(new Card(Card.Pattern.CLOVER, Card.Denomination.ACE));
+        gamer1.receiveCard(new Card(Card.Pattern.CLOVER, Card.Denomination.KING)); //== blackjek ==//
+
+        gamer2.receiveCard(new Card(Card.Pattern.CLOVER, Card.Denomination.ACE));
+        gamer2.receiveCard(new Card(Card.Pattern.CLOVER, Card.Denomination.KING)); //== Non BlackJek ==//
+
+        rule.firstBlackJekEvent(new Gamers(Arrays.asList(gamer1, gamer2)), dealer);
+
+        // then
+        Assertions.assertThat(gamer1.getPlayerMoney().getResultMoney()).isEqualTo(15000);
+
+
     }
 }
