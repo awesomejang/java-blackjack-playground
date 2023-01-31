99fc9e4910d1efdc4441706122091deff5f11328
Sangyun.Jang <awesomejang@gmail.com>
2023-01-31 18:08:42 +0900

    test : dealer.isReceiveCardTest 테스트케이스 추가

--- src/test/java/nextstep/blackjack/DealerTest.java
+++ src/test/java/nextstep/blackjack/DealerTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 
 public class DealerTest {
 
+    Dealer dealer = new Dealer();
+
 
     @Test
     @DisplayName("딜러는 카드의 총 합이 16이상이면 더 이상 카드를 받을 수 없다.")
     void receiveCardTest() {
-        Dealer dealer = new Dealer();
+        //Dealer dealer = new Dealer();
         dealer.receiveCard(new Card(Card.Pattern.CLOVER, Card.Denomination.JACK));
         dealer.receiveCard(new Card(Card.Pattern.CLOVER, Card.Denomination.SEVEN));
         dealer.receiveCard(new Card(Card.Pattern.CLOVER, Card.Denomination.ACE));
@@ -18,8 +20,16 @@ public class DealerTest {
         Assertions.assertThat(dealer.openCards().getCardSize()).isEqualTo(2);
     }
 
-    /*@Test
-    void isReceiveCard()*/
+    @Test
+    void isReceiveCardTest() {
+        dealer.receiveCard(new Card(Card.Pattern.CLOVER, Card.Denomination.JACK));
+        dealer.receiveCard(new Card(Card.Pattern.CLOVER, Card.Denomination.FIVE));
+        Assertions.assertThat(dealer.isReceiveCard()).isTrue();
+
+        dealer.receiveCard(new Card(Card.Pattern.CLOVER, Card.Denomination.FIVE));
+        Assertions.assertThat(dealer.isReceiveCard()).isFalse();
 
 
+
+    }
 }
