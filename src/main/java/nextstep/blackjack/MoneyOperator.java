f1750859097b74b296479f0a362705103aa4ab9f
awesomejang <uljang1995@gmail.com>
2023-02-02 23:05:31 +0900

    fix : MoneyOperator 계산식 추가

--- src/main/java/nextstep/blackjack/MoneyOperator.java
+++ src/main/java/nextstep/blackjack/MoneyOperator.java
@@ -1,5 +1,24 @@
 package nextstep.blackjack;
 
+import java.math.BigDecimal;
+import java.util.function.BiFunction;
+
 public enum MoneyOperator {
-    PLUS, MINUS, MULTIPLY;
+    PLUS((num1, num2) -> num2 + num1),
+    MINUS((num1, num2) -> num2 - num1),
+    MULTIPLY((num1, num2)-> new BigDecimal(num2).multiply(new BigDecimal(num1)).intValue());
+
+//    private String operator;
+    private BiFunction<Integer, Integer, Integer> biFunction;
+
+    MoneyOperator(BiFunction<Integer, Integer, Integer> biFunction) {
+        this.biFunction = biFunction;
+    }
+
+    public Integer calculate(int num1, int num2) {
+        return this.biFunction.apply(num1, num2);
+
+
+    }
+
 }
--- src/test/java/nextstep/blackjack/PlayerMoneyTest.java
+++ src/test/java/nextstep/blackjack/PlayerMoneyTest.java
@@ -7,11 +7,14 @@ import org.junit.jupiter.params.provider.EnumSource;
 public class PlayerMoneyTest {
 
     //@Test
-    @ParameterizedTest
+//    @ParameterizedTest
     //@EnumSource(value = MoneyOperator.class, names = {"PLUS", "MINUS", "MULTIPLY"})
-    @EnumSource(MoneyOperator.class)
-    void calculateResultMoneyTest(MoneyOperator moneyOperator) {
-        System.out.println(moneyOperator.name());
+//    @EnumSource(MoneyOperator.class)
+    @Test
+    void calculateResultMoneyTest() {
+        Integer calculate = MoneyOperator.PLUS.calculate(10, 20);
+        System.out.println(calculate.intValue());
+
 
         //PlayerMoney playerMoney = new PlayerMoney(10000);
 
