dc0807ae311aebe4f3b880e6dfe0fbf2792b86b0
Sangyun.Jang <uljang1995@gmail.com>
2023-02-15 17:38:31 +0900

    test : push 테스트 추가

--- src/test/java/nextstep/blackjack/CardTest.java
+++ src/test/java/nextstep/blackjack/CardTest.java
@@ -1,8 +1,12 @@
 package nextstep.blackjack;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CardTest {
     @Test
@@ -10,13 +14,20 @@ public class CardTest {
     void CardCreateTest() {
         // given
         Card.Pattern diamond = Card.Pattern.DIAMOND;
-        Card.Denomination four = Card.Denomination.FOUR;
-        // when
 
+        Card.Denomination four = Card.Denomination.FOUR;
+
+        // when
         Card card = new Card(diamond, four);
+
         // then
         Assertions.assertThat(card.getPattern()).isEqualTo(diamond);
         Assertions.assertThat(card.getDenomination()).isEqualTo(four);
         Assertions.assertThat(card.getDenomination().getPoint()).isEqualTo(four.getPoint());
+        test(3, "asd");
+    }
+
+    void test(int n, String s, Object... objects) {
+
     }
 }
